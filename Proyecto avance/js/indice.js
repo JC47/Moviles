0f8134772ee2be54da21f3f9e1e33d2ae618215a
Geromon <yeyochan_@hotmail.com>
2018-04-02 11:34:56 -0500

    Add files via upload

---
+++ Proyecto avance/js/indice.js
@@ -0,0 +1,28 @@
+var db;
+var log = document.getElementById('log');
+db = openDatabase("DBTest", "1.0", "BD Ejemplo", 200000);
+createTable();
+
+function createTable() {
+	db.transaction(function(tx) {
+		tx.executeSql("CREATE TABLE IF NOT EXISTS ejemplo (id INTEGER PRIMARY KEY ASC, nombre TEXT, movimiento TEXT, tiempo TEXT);", [], function(tx) {
+			log.innerHTML = 'Tabla Creada'
+		}, onError);
+	});
+}
+
+function onError(tx, error) {
+	log.innerHTML += '<p>' +error.message + '</p>';
+}
+
+function newRecord(nombre) {
+	//var num = Math.round(Math.random() * 10000); //random data
+	db.transaction(function(tx) {
+		tx.executeSql("INSERT INTO ejemplo (nombre, movimiento, tiempo) VALUES(?, ?, ?);", [nombre, 'valor nulo', 'valor nulo'], null, onError);
+	});
+}
+
+function registrar() {
+	var aux = document.getElementById("nombre").value;
+	newRecord(aux);
+}
