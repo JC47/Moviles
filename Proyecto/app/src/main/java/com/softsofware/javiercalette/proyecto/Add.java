db111ab0117799c8f0cdc2b06a4edc29aa3bb050
JC47 <javiercalette@gmail.com>
2018-06-05 19:21:18 -0500

    Formulario terminado (sin mapa)

--- Proyecto/app/src/main/java/com/softsofware/javiercalette/proyecto/Add.java
+++ Proyecto/app/src/main/java/com/softsofware/javiercalette/proyecto/Add.java
@@ -1,9 +1,17 @@
 package com.softsofware.javiercalette.proyecto;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Add extends AppCompatActivity {
 
@@ -23,5 +31,42 @@ public class Add extends AppCompatActivity {
                 R.array.states, android.R.layout.simple_spinner_item);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter1);
+
+        Spinner aux = (Spinner)findViewById(R.id.contacts);
+        ArrayList<String> con = this.getDatos();
+        ArrayAdapter<String> s3 =  new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, con);
+        aux.setAdapter(s3);
+    }
+
+    public ArrayList<String> getDatos(){
+
+        String[] r = new String[]{
+                ContactsContract.Data._ID,
+                ContactsContract.Data.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER,
+                ContactsContract.CommonDataKinds.Phone.TYPE
+        };
+
+        String query = ContactsContract.Data.MIMETYPE + "='"+
+                ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "' AND "
+                + ContactsContract.CommonDataKinds.Phone.NUMBER + " IS NOT NULL";
+
+        String s = ContactsContract.Data.DISPLAY_NAME + " ASC";
+
+        Cursor cContacto = getContentResolver().query(
+                ContactsContract.Data.CONTENT_URI,
+                r,
+                query,
+                null,
+                s
+        );
+
+        ArrayList<String> d = new ArrayList<>();
+
+        while(cContacto.moveToNext()){
+            d.add(cContacto.getString(1));
+        }
+
+        return d;
     }
 }
